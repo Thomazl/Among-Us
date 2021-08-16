047a4e3ef89a5cf66e6f543b1bf42ba1aeca982e
Thomazl <luan.abrantes1999@gmail.com>
2021-08-16 09:33:44 -0300

    Editando para subir no git pages

--- Among/among.js
+++
@@ -1,9 +0,0 @@
-function movein() {
-    var among1 = document.getElementById('amongus')
-    among1.style.display = 'block'
-}
-
-function moveout() {
-    var among1 = document.getElementById('amongus')
-    among1.style.display = 'none'
-}
---
+++ among.js
@@ -0,0 +1,9 @@
+function movein() {
+    var among1 = document.getElementById('amongus')
+    among1.style.display = 'block'
+}
+
+function moveout() {
+    var among1 = document.getElementById('amongus')
+    among1.style.display = 'none'
+}
